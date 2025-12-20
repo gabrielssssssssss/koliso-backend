d3eb691883d7e5796f889afd3bee084db5bd5991
gabrielssssssssss <974gabriel.contact@gmail.com>
2025-12-20 18:54:23 +0100

    JWT validation finished

--- internal/middlewares/config.go
+++ internal/middlewares/config.go
@@ -3,11 +3,23 @@ package middlewares
 import (
 	"fmt"
 
+	"github.com/golang-jwt/jwt/v4"
+
 	"github.com/gin-gonic/gin"
 )
 
 func VerifyJwtToken(c *gin.Context) {
 	c.Next()
 	jwtToken := c.Request.Header.Get("JWT_TOKEN")
-	fmt.Println(jwtToken)
+	token, _ := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return JWT_SECRET_KEY, nil
+	})
+
+	if token.Valid == true {
+		c.JSON(401, fmt.Errorf("JWT validation error: signature verification failed, invalid signing key"))
+	}
+
 }
